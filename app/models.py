5096d0a70eda73e7f0ed5a96d36227d2340a16d8
Pedro Meira <pedrohenrique.peu09@gmail.com>
2021-09-07 19:05:56 -0300

    Implementação das funcionalidades de login e cadastro

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class Usuario(db.Model):
-    id_usuario = db.Column(db.Integer, primary_key=True)
+@login.user_loader
+def load_usuario(id):
+    return Usuario.query.get(int(id))
+
+class Usuario(UserMixin, db.Model):
+    # Para que o FlaskLogin funcione, a PK do usuário deve ter o nome "id" apenas.
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), index=True)
     email = db.Column(db.String(100), index=True, unique=True)
     senha = db.Column(db.String(128))
@@ -11,6 +18,13 @@ class Usuario(db.Model):
 
     def __repr__(self):
         return f"<Usuario {self.nome}>"
+
+    def criptografar_senha(self, senha):
+        self.senha = generate_password_hash(senha)
+
+    def checar_senha(self, senha):
+        return check_password_hash(self.senha, senha)
+
 
 class Barbeiro(db.Model):
     id_barbeiro = db.Column(db.Integer, primary_key=True)
@@ -38,7 +52,7 @@ class Reserva(db.Model):
     horario_inicio = db.Column(db.Time)
     horario_fim = db.Column(db.Time)
     data = db.Column(db.Date)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
     barbeiro_id = db.Column(db.Integer, db.ForeignKey('barbeiro.id_barbeiro'))
     servico_id = db.Column(db.Integer, db.ForeignKey('servico.id_servico'))
     
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,64 @@
-from flask import render_template
-from app import app
-from app.forms import LoginForm
+from flask import render_template, redirect, url_for, flash, request
+from app import app, db
+from app.forms import LoginForm, CadastrarUsuario
+from app.models import Usuario
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
+# HOME -------------------------------------------------
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html', titulo="Home")
 
-@app.route('/autenticacao', methods=['GET', 'POST'])
+# AUTENTICAÇÃO  -----------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Verifica se o usuário já está logado
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
     form = LoginForm()
+    # Verifica se passou nas validações do FlaskForm
+    if form.validate_on_submit():
+        # Busca o usuário com o e-mail informado no banco
+        usuario = Usuario.query.filter_by(email=form.email.data).first()
+        # Verifica se o usuário existe e se a senha está correta
+        if usuario is None or not usuario.checar_senha(form.senha.data):
+            flash('E-mail ou senha inválido(s).')
+            return redirect(url_for('login'))
+        # Realiza o login
+        login_user(usuario, remember=form.lembrar_me.data)
+        # Verifica se houve uma página requisitada restrita pelo login
+        pagina_requisitada = request.args.get('next')
+        if not pagina_requisitada or url_parse(pagina_requisitada).netloc != '':
+            pagina_requisitada = url_for('index')
+        return redirect(pagina_requisitada)
+
     return render_template('login.html', titulo="Login", form=form)
 
-# Usuarios
-@app.route('/cadastro')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# USUÁRIOS -----------------------------------------
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-    return render_template('cadastrar_usuario.html', titulo="Cadastre-se!")
+    # Verifica se o usuário já está logado
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = CadastrarUsuario()
+    # Se passar na validação de formulário faça a lógica abaixo
+    if form.validate_on_submit():
+        # Popula um objeto de Usuario
+        usuario = Usuario(nome=form.nome.data,  email=form.email.data)
+        usuario.criptografar_senha(form.senha.data)
+        # Adiciona ao banco
+        db.session.add(usuario)
+        db.session.commit()
+        flash('Sua conta foi criada com sucesso!')
+        return redirect(url_for('login'))
+    return render_template('cadastrar_usuario.html', titulo="Crie sua conta: ", form=form)
